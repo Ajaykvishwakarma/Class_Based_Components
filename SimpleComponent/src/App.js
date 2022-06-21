2ae8af60609ac89cf1aaecd9e8c7fc0ffd0c6d61
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-06-21 18:18:10 +0530

    O-one

---
+++ SimpleComponent/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  Car  from './Components/Car';
+
+function App() {
+  return (
+    <div className="App">
+     <Car />
+    </div>
+  );
+}
+
+export default App;
---
+++ SimpleComponent/src/Components/Car.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import  ReactDOM  from 'react-dom/client';
+
+class Car extends React.Component {
+    render() {
+        return(
+            <div>
+                <h2>Car is mine::::::</h2>
+            </div>
+        )
+    }
+}
+
+export default Car
+
+
+
